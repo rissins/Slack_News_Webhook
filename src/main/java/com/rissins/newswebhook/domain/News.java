e54a8e69233d6b856147fef10d2e3051b8fc5182
rissins <tnalsqqq@naver.com>
2022-06-11 17:36:23 +0900

    feat: create born

---
+++ src/main/java/com/rissins/newswebhook/domain/News.java
@@ -0,0 +1,24 @@
+package com.rissins.newswebhook.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "news")
+public class News {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String title;
+
+    String content;
+}
