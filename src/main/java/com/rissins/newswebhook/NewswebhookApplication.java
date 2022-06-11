602e67d9e2619925633cf0bdb409afdcbeae4714
rissins <tnalsqqq@naver.com>
2022-06-11 17:13:14 +0900

    initial commit

---
+++ src/main/java/com/rissins/newswebhook/NewswebhookApplication.java
@@ -0,0 +1,13 @@
+package com.rissins.newswebhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewswebhookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewswebhookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rissins/newswebhook/NewswebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rissins.newswebhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewswebhookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
