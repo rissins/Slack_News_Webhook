594d70d1b64222d739ea4dacac6c0500e8043160
rissins <tnalsqqq@naver.com>
2022-06-12 16:27:54 +0900

    refactor: url 주소변경 ( 최신순으로 )

--- src/main/java/com/rissins/newswebhook/util/crawling/Site.java
+++ src/main/java/com/rissins/newswebhook/util/crawling/Site.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum Site {
 
-    NAVER("https://search.naver.com/search.naver?where=news&sm=tab_pge&query=속보&start=", "news_tit", "news_dsc");
+    NAVER("https://search.naver.com/search.naver?where=news&sm=tab_pge&query=개발자&nso=so%3Add%2Cp%3Aall&is_sug_officeid=0", "news_tit", "news_dsc");
 
     private String url;
     private String titleClassName;
