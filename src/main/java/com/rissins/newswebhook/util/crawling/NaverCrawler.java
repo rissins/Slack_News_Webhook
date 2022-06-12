a491561b461d51172a2d2e2cc515cb6e86cb8f06
rissins <tnalsqqq@naver.com>
2022-06-12 17:08:53 +0900

    refactor: insert 시 url 추가

--- src/main/java/com/rissins/newswebhook/util/crawling/NaverCrawler.java
+++ src/main/java/com/rissins/newswebhook/util/crawling/NaverCrawler.java
@@ -21,9 +21,12 @@ public class NaverCrawler implements Crawler {
         Document html = conn.get();
         Element newsTitleElement = html.getElementsByClass(Site.NAVER.getTitleClassName()).get(0);
         Element newsContentElement = html.getElementsByClass(Site.NAVER.getContentClassName()).get(0);
+        String newsUrlElement = newsTitleElement.attr("href");
 
         return News.builder()
                 .title(newsTitleElement.text())
-                .content(newsContentElement.text()).build();
+                .content(newsContentElement.text())
+                .url(newsUrlElement)
+                .build();
     }
 }
