10cfbaffdff2453095096624f10ab8c0189423be
rissins <tnalsqqq@naver.com>
2022-06-15 01:51:17 +0900

    design: blank

--- src/main/java/com/rissins/newswebhook/NewswebhookApplication.java
+++ src/main/java/com/rissins/newswebhook/NewswebhookApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableBatchProcessing
 public class NewswebhookApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(NewswebhookApplication.class, args);
 	}
